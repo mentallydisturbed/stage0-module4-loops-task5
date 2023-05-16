999c095facb755dab08e07ead4fea6f8d7d3603b
mentallydisturbed <magzhan.zhumagazy@gmail.com>
2023-05-17 01:36:01 +0600

    mod4task5

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
+        for(int i = 1; i <= height; i++) {
+            for(int j = 1; j <= height; j++) {
+                if(i == 1 || i == height || i == j || height - j + 1 == i)
+                    System.out.print(8);
+                else System.out.print(' ');
+            }
+            System.out.println();
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task5;
 
 public class Rectangle {
     public void printRectangleFrom8s(int length, int height){
-
+        for(int i = 1; i <= height; i++) {
+            for(int j = 1; j <= length; j++) {
+                if(j == 1 || j == length) System.out.print(8);
+                else if(i == 1 || i == height) System.out.print(8);
+                else System.out.print(' ');
+            }
+            System.out.println();
+        }
     }
 }
