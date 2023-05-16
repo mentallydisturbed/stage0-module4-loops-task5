0844088ba7a89d49276a449a5dbde379c1c3a2d2
mentallydisturbed <magzhan.zhumagazy@gmail.com>
2023-05-17 01:41:19 +0600

    fix2mod4task5

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -4,11 +4,15 @@ public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
         for(int i = 1; i <= height; i++) {
             for(int j = 1; j <= height; j++) {
-                if(i == 1 || i == height || i == j || height - j + 1 == i)
+                if(i == 1 || i == height || (i <= j && height - j + 1 >= i) || (i >= j && height - j + 1 <= i))
                     System.out.print(8);
                 else System.out.print(' ');
             }
             System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+        new Hourglass().printHourglassOfGivenSize(6);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -4,7 +4,7 @@ public class Triangle {
     public void printTriangle(int cathetusLength) {
         for(int i = 1; i <= cathetusLength; i++) {
             for (int j = 1; j <= i; j++) {
-                if(i == 1 || j == 1 || i == cathetusLength || i == j) System.out.print(8);
+                if(i == 1 || j == 1 || i == cathetusLength || i >= j) System.out.print(8);
                 else System.out.print(' ');
             }
             System.out.println();
